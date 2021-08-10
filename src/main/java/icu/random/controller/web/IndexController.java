1719a95b3f5e9d708847aeb05b0da23935843e38
pythoninja <pythoninja@users.noreply.github.com>
2021-08-10 21:27:15 +0300

    refactor: allow only GET method on `/`

--- src/main/java/icu/random/controller/web/IndexController.java
+++ src/main/java/icu/random/controller/web/IndexController.java
@@ -3,11 +3,12 @@ package icu.random.controller.web;
 import static icu.random.api.WebControllerPaths.WEB_INDEX_PATH;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
-  @RequestMapping(WEB_INDEX_PATH)
+  @RequestMapping(value = WEB_INDEX_PATH, method = RequestMethod.GET)
   public String index() {
     return "index";
   }
