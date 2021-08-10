455ab180dfc6334c1ae395cf8a355937077b719e
pythoninja <pythoninja@users.noreply.github.com>
2021-08-10 21:39:32 +0300

    refactor: change to GetMapping shortcut

--- src/main/java/icu/random/controller/web/IndexController.java
+++ src/main/java/icu/random/controller/web/IndexController.java
@@ -2,13 +2,12 @@ package icu.random.controller.web;
 
 import static icu.random.api.WebControllerPaths.WEB_INDEX_PATH;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-  @RequestMapping(value = WEB_INDEX_PATH, method = RequestMethod.GET)
+  @GetMapping(WEB_INDEX_PATH)
   public String index() {
     return "index";
   }
