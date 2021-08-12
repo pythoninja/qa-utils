d2b99d4cd1de63361206bf0d1f93ed517b11e464
pythoninja <pythoninja@users.noreply.github.com>
2021-08-12 19:01:15 +0300

    refactor: replace lombok data with explicit annotations
    This commit fixes linter warning about Hibernate and Lombok's @Data annotation with entities.

--- src/main/java/icu/random/entity/Event.java
+++ src/main/java/icu/random/entity/Event.java
@@ -2,6 +2,7 @@ package icu.random.entity;
 
 import icu.random.util.event.EventType;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -10,10 +11,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 
 @Entity(name = "Event")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Event {
 
   @Id
@@ -38,6 +46,17 @@ public class Event {
     this.eventType = eventType;
   }
 
-  public Event() {
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+    Event event = (Event) o;
+
+    return Objects.equals(id, event.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return 1491041522;
   }
 }
